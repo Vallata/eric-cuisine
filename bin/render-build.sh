76faba8552fb6c1d96048314a6de6e028db7c84e
Alizee Vallat <alizee.v@live.fr>
2026-04-14 11:18:20 +0200

    Fix assets precompile command

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,9 +3,11 @@
 set -o errexit
 
 bundle install
+
+# On utilise 'bundle exec rails' pour être sûr de l'environnement
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-# Si tu n'as pas encore de base de données configurée,
-# tu peux commenter la ligne suivante avec un #,
-# mais normalement elle est nécessaire :
+
+# Si tu as une base de données sur Render, garde cette ligne.
+# Si tu n'as pas encore créé de base Postgres sur Render, commente-la avec un #
 bundle exec rails db:migrate
