402d5d03af5f60ef90c4ce7b47adb42f0f08e114
Alizee Vallat <alizee.v@live.fr>
2026-04-14 11:13:06 +0200

    Upgrade to Ruby 3.3.0 and fix build script

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,10 +2,10 @@
 # exit on error
 set -o errexit
 
-# AJOUTE CETTE LIGNE : elle règle le problème de compatibilité nokogiri
-gem update --system --no-document
-
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
+# Si tu n'as pas encore de base de données configurée,
+# tu peux commenter la ligne suivante avec un #,
+# mais normalement elle est nécessaire :
 bundle exec rails db:migrate
