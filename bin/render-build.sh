620e1047a8952fc6f70a7707bc4c1c00e49b3dc0
Alizee Vallat <alizee.v@live.fr>
2026-04-14 11:10:23 +0200

    Fix: Update RubyGems and platforms for Render

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,9 @@
 # exit on error
 set -o errexit
 
+# AJOUTE CETTE LIGNE : elle règle le problème de compatibilité nokogiri
+gem update --system --no-document
+
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
